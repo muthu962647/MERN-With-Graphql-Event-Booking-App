a57e92841fff981078dd5cdf82efaf5a60b7515a
Muthukumar Balasubramanian <129060533+muthu962647@users.noreply.github.com>
2024-01-28 21:54:49 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema =new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Event'
+        }
+    ]
+});
+
+mongoose.model('User',userSchema);
+
+module.exports = mongoose.model('User');
